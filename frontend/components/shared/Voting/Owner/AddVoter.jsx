22b6666da08e88c663fc29f76699caae38e36137
JonaPlaz <jplazanet@hotmail.com>
2024-11-14 14:42:23 +0100

    feat(owner) update AddVoter with alerts

--- frontend/components/shared/Voting/Owner/AddVoter.jsx
+++ frontend/components/shared/Voting/Owner/AddVoter.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useToast } from "@/hooks/use-toast";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 import { useWriteContract, useWaitForTransactionReceipt } from "wagmi";
 
@@ -15,9 +15,6 @@ import { contractAddress, contractAbi } from "@/constants";
 // 0x90F79bf6EB2c4f870365E785982E1f101E93b906
 const AddVoter = () => {
   const [address, setAddress] = useState("");
-  const [event, setEvent] = useState([]);
-
-  const { toast } = useToast();
 
   const { data: hash, error, isPending: addIsPending, writeContract } = useWriteContract({});
 
@@ -34,15 +31,46 @@ const AddVoter = () => {
 
   return (
     <>
-      <h2 className="text-2xl">Ajouter un votant</h2>
-      <div className="flex items-center">
-        <Input placeholder="Adresse" onChange={(e) => setAddress(e.target.value)} value={address} />
-        <Button disabled={addIsPending} onClick={handleAddVoter}>
-          {addIsPending ? "En cours d'ajout" : "Add"}
-        </Button>
-      </div>
-      <div className="flex items">
-        <p>Event</p>
+      <div className="mb-8">
+        <h2 className="text-2xl mb-4">Ajouter un votant</h2>
+        <div className="flex flex-col w-full">
+          {hash && (
+            <Alert className="mb-4 bg-lime-200">
+              <AlertTitle>Information</AlertTitle>
+              <AlertDescription>Transaction Hash : {hash}</AlertDescription>
+            </Alert>
+          )}
+          {isConfirming && (
+            <Alert className="mb-4 bg-lime-200">
+              <AlertTitle>Information</AlertTitle>
+              <AlertDescription>Waiting for confirmation...</AlertDescription>
+            </Alert>
+          )}
+          {isSuccess && (
+            <Alert className="mb-4 bg-lime-200">
+              <AlertTitle>Information</AlertTitle>
+              <AlertDescription>Transaction Confirmed.</AlertDescription>
+            </Alert>
+          )}
+          {erroConfirmation && (
+            <Alert className="mb-4 bg-lime-200">
+              <AlertTitle>Error</AlertTitle>
+              <AlertDescription>{erroConfirmation.shortMessage || erroConfirmation.message}</AlertDescription>
+            </Alert>
+          )}
+          {error && (
+            <Alert className="mb-4 bg-lime-200">
+              <AlertTitle>Error</AlertTitle>
+              <AlertDescription>{error.shortMessage || error.message}</AlertDescription>
+            </Alert>
+          )}
+        </div>
+        <div className="flex items-center">
+          <Input placeholder="Adresse" onChange={(e) => setAddress(e.target.value)} value={address} />
+          <Button disabled={addIsPending} onClick={handleAddVoter}>
+            {addIsPending ? "En cours d'ajout" : "Add"}
+          </Button>
+        </div>
       </div>
     </>
   );
